fa5a971555b0ec1a60e8bc854dc5a02765f54fc5
Jakub Zmuda <kuba.zm18@gmail.com>
2023-03-17 19:39:02 +0100

    Add exemplary kotlin app

---
+++ backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package pl.poznan.put.tsd.planningpoker.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/pl/poznan/put/tsd/planningpoker/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.poznan.put.tsd.planningpoker.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
