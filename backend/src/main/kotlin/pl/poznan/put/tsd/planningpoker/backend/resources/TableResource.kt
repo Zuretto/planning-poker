ee1d369ccb60d2ee0eb823b398d19c3b8a30f5b3
Zuretto <mikzur4@gmail.com>
2023-03-23 11:05:57 +0100

    add CrossOrigin annotation on Table Resource

--- backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/resources/TableResource.kt
+++ backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/resources/TableResource.kt
@@ -2,12 +2,14 @@ package pl.poznan.put.tsd.planningpoker.backend.resources
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import pl.poznan.put.tsd.planningpoker.backend.resources.responses.TableCreatedResponse
 import pl.poznan.put.tsd.planningpoker.backend.services.GamesService
 
+@CrossOrigin
 @RestController
 class TableResource constructor(private val games: GamesService) {
     @PostMapping("table")
