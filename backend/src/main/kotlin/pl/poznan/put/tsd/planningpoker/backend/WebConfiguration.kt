02ebfb4f1fa5c4a94d7a2612cac14c42ac7a9b6e
Wojtek <wojciechkrystyniak@gmail.com>
2023-04-01 17:55:24 +0200

    Disable cors

---
+++ backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/WebConfiguration.kt
@@ -0,0 +1,12 @@
+package pl.poznan.put.tsd.planningpoker.backend
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
+    }
+}
