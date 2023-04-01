0ac1afe7f08afecb39da6fb388b959b60b67459a
Jakub Zmuda <kuba.zm18@gmail.com>
2023-04-01 20:16:12 +0200

    Fix post method body

--- frontend/src/lib/util/api-handler.ts
+++ frontend/src/lib/util/api-handler.ts
@@ -6,7 +6,13 @@ const baseUrl = import.meta.env.VITE_BASE_URL;
 export const createTable = (nickname: string): Promise<TableResponse> => {
     return fetch(`${baseUrl}/poker_api/v1/table`, {
         method: 'POST',
-        body: nickname,
+        body: JSON.stringify({
+            "username": nickname
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        }
     })
         .then(response => response.json())
         .then(config => config as TableResponse);
