cdf30bef3e97253d3d16ed8033cf19f4668c3d71
Zuretto <mikzur4@gmail.com>
2023-04-16 14:14:35 +0200

    add display of the game table functionality

---
+++ frontend/src/lib/components/TableView/ReadOnlyCard.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    import type { PlayerResponse } from "../../util/api-handler.models";
+    import checkmarkIcon from '../../../assets/check-mark.svg';
+    import { Card, cardEnumToDisplayText } from "../../util/enums";
+
+    export let isSelectionVisible;
+    export let player: PlayerResponse;
+</script>
+
+<div class="read-only-card-wrapper">
+    <div class="read-only-card-card">
+        {#if !isSelectionVisible && player.selectedCard !== Card.NONE}
+            <img class="checkmark-icon"
+                 src="{checkmarkIcon}"
+                 alt="Checkmark Icon"/>
+        {:else if isSelectionVisible}
+            <span class="selected-estimation">
+                {cardEnumToDisplayText[player.selectedCard]}
+            </span>
+        {/if}
+
+    </div>
+    <span class="read-only-card-caption">
+        {player.name}
+    </span>
+</div>
+<!--TODO: card that is grey when -->
+
+<style lang="scss">
+
+  $cardWidth: 5rem;
+  $cardHeight: $cardWidth * 1.5;
+  $iconSide: $cardWidth * 0.8;
+  $iconPaddingTop: $cardWidth * 0.1;
+
+  .read-only-card-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .read-only-card-card {
+      background-color: whitesmoke;
+      width: $cardWidth;
+      height: $cardHeight;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border-radius: 8px;
+
+      .checkmark-icon {
+        width: $iconSide;
+        height: $iconSide;
+        // filter below is #646cff
+        filter: invert(52%) sepia(66%) saturate(5012%) hue-rotate(220deg) brightness(99%) contrast(105%);
+      }
+
+      .selected-estimation {
+        font-size: 1.5rem;
+      }
+    }
+
+    .read-only-card-caption {
+
+    }
+  }
+
+
+</style>
+
+
+
+
--- frontend/src/lib/components/TableView/TableView.svelte
+++ frontend/src/lib/components/TableView/TableView.svelte
@@ -1,14 +1,77 @@
 <script lang="ts">
+    import { establishWebsocketConnection } from "../../util/api-handler";
+    import type { PlayerResponse } from "../../util/api-handler.models";
+    import { onDestroy } from "svelte";
+    import ReadOnlyCard from "./ReadOnlyCard.svelte";
+
     export let username;
     export let tableId;
 
-    let players = [];
+    let areCardsVisible = false;
+    let players: PlayerResponse[] = [];
 
+    const closeWebsocket = establishWebsocketConnection(
+        username,
+        tableId,
+        (closeEvent) => {
+        },
+        (errorCloseEvent) => {/* TODO toast message */
+        },
+        (validationError) => {/* TODO toast message */
+        },
+        (message) => {
+            players = message.data.players;
+            areCardsVisible = message.data.areCardsVisible;
+        }
+    );
+
+    onDestroy(() => closeWebsocket());
 </script>
 
-
-
+<div class="circle-container">
+    <ul>
+        {#each players as player, i}
+            <li style="{`transform: translate(-50%, -50%) rotate(${(i * 360) / players.length}deg) translateY(-12rem) rotate(-${(i * 360) / players.length}deg)`}">
+                <ReadOnlyCard isSelectionVisible="{areCardsVisible}"
+                              player="{player}"/>
+            </li>
+        {/each}
+    </ul>
+</div>
 
 <style>
+    .circle-container {
+        position: relative;
+        display: flex;
+        place-items: center;
+        width: 24rem;
+        height: 24rem;
+        margin: 15px;
+        justify-content: center;
+        text-align: center;
+        border-radius: 50%;
+    }
 
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    ul > li {
+        display: block;
+        font-weight: bold;
+        text-align: center;
+        border-radius: 50%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        transition: transform .5s cubic-bezier(0.5, 0, 0.35, 1);
+        transform: translate(-50%, -50%);
+        transform-origin: center center;
+    }
 </style>
