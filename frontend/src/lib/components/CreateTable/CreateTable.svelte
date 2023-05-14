db10320672ef9458bf39cdca93448faa78b396e3
Wojtek <wojciechkrystyniak@gmail.com>
2023-05-14 17:08:30 +0200

    Upload file on frontend

--- frontend/src/lib/components/CreateTable/CreateTable.svelte
+++ frontend/src/lib/components/CreateTable/CreateTable.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import { createTable } from '../../util/api-handler';
+    import { createTable, uploadJiraCSV } from '../../util/api-handler';
     import { navigate } from "svelte-routing";
 
     let username = '';
+    let isFileUploadSelected = false;
+    let files: File;
+
     const createBoard = (): void => {
         if (!username || !username.trim().length) {
             return;
@@ -10,9 +13,16 @@
 
         createTable(username)
             .then((tableResponse) => {
+                if (isFileUploadSelected) {
+                    uploadJiraCSV(tableResponse.id, files[0])
+                }
+                return tableResponse
+            })
+            .then((tableResponse) => {
                 navigate(`table/${tableResponse.id}`);
             });
     };
+
 </script>
 
 <div class="text-column">
@@ -21,6 +31,10 @@
     <form on:submit|preventDefault={createBoard}>
         <input name="username" type="text" id="nickname-input" placeholder="Enter your nickname"
                bind:value={username}><br>
+        <input type="checkbox" bind:checked={isFileUploadSelected}> Import Jira File<br>
+        {#if isFileUploadSelected}
+            <input type="file" accept="text/csv" bind:files><br><br>
+        {/if}
         <input type="submit" value="Create board">
     </form>
 </div>
