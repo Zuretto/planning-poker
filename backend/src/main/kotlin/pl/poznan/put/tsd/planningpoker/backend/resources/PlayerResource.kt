4fb526bbb4584f1cb31147a5ab4fb7ec320754a2
Wojtek <wojciechkrystyniak@gmail.com>
2023-06-06 21:31:55 +0200

    Small refactor...

--- backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/resources/PlayerResource.kt
+++ backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/resources/PlayerResource.kt
@@ -4,17 +4,28 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import pl.poznan.put.tsd.planningpoker.backend.model.Player
 import pl.poznan.put.tsd.planningpoker.backend.resources.responses.GameHistoryResponse
+import pl.poznan.put.tsd.planningpoker.backend.services.GamesService
 import pl.poznan.put.tsd.planningpoker.backend.services.PlayersService
 
 @RestController
 class PlayerResource(
-    private val playersService: PlayersService
+    private val playersService: PlayersService,
+    private val gamesService: GamesService
 ) {
     @GetMapping("player/{username}/history")
     suspend fun getGamesHistory(@PathVariable username: String): ResponseEntity<List<GameHistoryResponse>> {
-        val games = playersService.getPlayerHistory(username)
+        val games = gamesService.getPlayerHistory(username)
         return ResponseEntity(games, HttpStatus.OK)
+    }
+
+    @PostMapping("register")
+    suspend fun register(@RequestBody player: Player): ResponseEntity<Unit> {
+        playersService.registerPlayer(player.name, player.password)
+        return ResponseEntity(Unit, HttpStatus.OK)
     }
 }
--- backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/services/PlayersService.kt
+++ backend/src/main/kotlin/pl/poznan/put/tsd/planningpoker/backend/services/PlayersService.kt
@@ -4,16 +4,11 @@ import org.springframework.stereotype.Service
 import pl.poznan.put.tsd.planningpoker.backend.model.Player
 import pl.poznan.put.tsd.planningpoker.backend.model.PlayerDoesNotExistException
 import pl.poznan.put.tsd.planningpoker.backend.model.UsernameTakenException
-import pl.poznan.put.tsd.planningpoker.backend.resources.responses.GameHistoryResponse
-import pl.poznan.put.tsd.planningpoker.backend.resources.responses.GameHistoryResponse.Companion.toResponseModel
-import kotlin.jvm.Throws
 
 @Service
-class PlayersService(
-    private val gamesService: GamesService
-) {
+class PlayersService {
 
-    val players: MutableList<Player> = mutableListOf()
+    private val players: MutableList<Player> = mutableListOf()
 
     @Throws(UsernameTakenException::class)
     suspend fun registerPlayer(username: String, password: String) {
@@ -21,12 +16,10 @@ class PlayersService(
             throw UsernameTakenException("Username: $username is already taken")
         else
             players.add(Player(username, password))
-        // TODO return JWT token
     }
 
     @Throws(PlayerDoesNotExistException::class)
-    suspend fun getPlayerHistory(username: String): List<GameHistoryResponse> {
-        val history = gamesService.playerGameConnections.filter { it.player.name == username }.map { it.game }
-        return history.map { it.toResponseModel(gamesService) }
+    suspend fun getPlayerForLogin(login: String): Player {
+        return players.find { it.name == login } ?: throw PlayerDoesNotExistException("Player with login: $login does not exist")
     }
 }
