ad60dc1abf0c193c272f81f52a6382e50e06b4e3
Wojtek <wojciechkrystyniak@gmail.com>
2023-06-12 17:01:14 +0200

    Fix tests

--- backend/src/test/kotlin/pl/poznan/put/tsd/planningpoker/backend/services/GamesServiceTest.kt
+++ backend/src/test/kotlin/pl/poznan/put/tsd/planningpoker/backend/services/GamesServiceTest.kt
@@ -1,6 +1,7 @@
 package pl.poznan.put.tsd.planningpoker.backend.services
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -23,7 +24,12 @@ class GamesServiceTest {
     }
     private val messageHandler = mock(MessageHandler::class.java) // TODO test properly...
     private val csvParser = mock(CsvParser::class.java) // TODO test properly...
-    private val service = GamesService(uuidProvider, messageHandler, csvParser, PlayersService())
+    private val service = GamesService(uuidProvider, messageHandler, csvParser, PlayersService().apply {
+        runBlocking {
+            registerPlayer("username", "password")
+            registerPlayer("username2", "password")
+        }
+    })
 
     @Test
     fun `Given creators name when creating a table then return uuid`() = runTest {
